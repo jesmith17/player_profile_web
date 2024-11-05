35106d8acffce32e04bfa7cae595f5ed7570d416
Josh Smith <jesmith17@gmail.com>
2024-11-05 08:21:27 -0600

    Changed API calls to be HTTP

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: "https://ec2-34-208-187-189.us-west-2.compute.amazonaws.com:8080"
+    apiUrl: "http://ec2-34-208-187-189.us-west-2.compute.amazonaws.com:8080"
   };
