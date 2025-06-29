d01bc1b06778aa46e5fdf79d417d1433560b744b
Josh Smith <jesmith17@gmail.com>
2025-06-29 09:44:52 -0500

    Updated env for the new URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: "https://us-central1-player-profile-445504.cloudfunctions.net/nodejs-http-functions/api"
+    apiUrl: "https://us-central1-player-profile-daa1a.cloudfunctions.net/player-profile-svc/api"
   };
